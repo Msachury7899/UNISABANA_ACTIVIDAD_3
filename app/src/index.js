7ef25586c1c2ca280a01d05d886e200a978480e2
Msachury <msachury7899@gmail.com>
2026-06-23 00:24:44 -0500

    fix: actualizar mensaje en la ruta /api/items2 a 'Probando automatizacion 6'

--- app/src/index.js
+++ app/src/index.js
@@ -29,7 +29,7 @@ app.get('/api/items', (req, res) => {
 });
 
 app.get('/api/items2', (req, res) => {
-  res.json({items, message: 'Probando automatizacion 5'});
+  res.json({items, message: 'Probando automatizacion 6'});
 });
 
 app.get('/api/items/:id', (req, res) => {
